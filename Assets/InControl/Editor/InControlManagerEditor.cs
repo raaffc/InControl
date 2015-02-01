18d4c4a91637b05fb64e28e10eed8ce67a8e3818
Robert Acksel <robert.acksel@googlemail.com>
2015-02-01 22:49:18 +0100

    --some additional popup to select known user input types to input manager

--- Assets/InControl/Editor/InControlManagerEditor.cs
+++ Assets/InControl/Editor/InControlManagerEditor.cs
@@ -1,63 +1,82 @@
 #if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using InControl.ReorderableList;
 
+namespace InControl{
+    [CustomEditor(typeof (InControlManager))]
+    public class InControlManagerEditor : Editor{
+        private SerializedProperty logDebugInfo;
+        private SerializedProperty invertYAxis;
+        private SerializedProperty enableXInput;
+        private SerializedProperty useFixedUpdate;
+        private SerializedProperty dontDestroyOnLoad;
+        private SerializedProperty customProfiles;
+        private Texture headerTexture;
 
-namespace InControl
-{
-	[CustomEditor( typeof(InControlManager) )]
-	public class InControlManagerEditor : Editor
-	{
-		SerializedProperty logDebugInfo;
-		SerializedProperty invertYAxis;
-		SerializedProperty enableXInput;
-		SerializedProperty useFixedUpdate;
-		SerializedProperty dontDestroyOnLoad;
-		SerializedProperty customProfiles;
-		Texture headerTexture;
-		
+        private void OnEnable(){
+            logDebugInfo = serializedObject.FindProperty("logDebugInfo");
+            invertYAxis = serializedObject.FindProperty("invertYAxis");
+            enableXInput = serializedObject.FindProperty("enableXInput");
+            useFixedUpdate = serializedObject.FindProperty("useFixedUpdate");
+            dontDestroyOnLoad = serializedObject.FindProperty("dontDestroyOnLoad");
+            customProfiles = serializedObject.FindProperty("customProfiles");
 
-		void OnEnable()
-		{
-			logDebugInfo = serializedObject.FindProperty( "logDebugInfo" );
-			invertYAxis = serializedObject.FindProperty( "invertYAxis" );
-			enableXInput = serializedObject.FindProperty( "enableXInput" );
-			useFixedUpdate = serializedObject.FindProperty( "useFixedUpdate" );
-			dontDestroyOnLoad = serializedObject.FindProperty( "dontDestroyOnLoad" );
-			customProfiles = serializedObject.FindProperty( "customProfiles" );
+            var path = AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this));
+            headerTexture = Resources.LoadAssetAtPath<Texture>(Path.GetDirectoryName(path) + "/Images/InControlHeader.png");
+        }
 
-			var path = AssetDatabase.GetAssetPath( MonoScript.FromScriptableObject( this ) );
-			headerTexture = Resources.LoadAssetAtPath<Texture>( Path.GetDirectoryName( path ) + "/Images/InControlHeader.png" );
-		}
+        public override void OnInspectorGUI(){
+            serializedObject.Update();
 
+            GUILayout.Space(5.0f);
 
-		public override void OnInspectorGUI()
-		{
-			serializedObject.Update();
+            var headerRect = GUILayoutUtility.GetRect(0.0f, 5.0f);
+            headerRect.width = headerTexture.width;
+            headerRect.height = headerTexture.height;
+            GUILayout.Space(headerRect.height);
+            GUI.DrawTexture(headerRect, headerTexture);
 
-			GUILayout.Space( 5.0f );
+            logDebugInfo.boolValue = EditorGUILayout.ToggleLeft("Log Debug Info", logDebugInfo.boolValue);
+            invertYAxis.boolValue = EditorGUILayout.ToggleLeft("Invert Y Axis", invertYAxis.boolValue);
+            enableXInput.boolValue = EditorGUILayout.ToggleLeft("Enable XInput (Windows)", enableXInput.boolValue);
+            useFixedUpdate.boolValue = EditorGUILayout.ToggleLeft("Use Fixed Update", useFixedUpdate.boolValue);
+            dontDestroyOnLoad.boolValue = EditorGUILayout.ToggleLeft("Don't Destroy On Load", dontDestroyOnLoad.boolValue);
 
-			var headerRect = GUILayoutUtility.GetRect( 0.0f, 5.0f );
-			headerRect.width = headerTexture.width;
-			headerRect.height = headerTexture.height;
-			GUILayout.Space( headerRect.height );
-			GUI.DrawTexture( headerRect, headerTexture );
+            ReorderableListGUI.Title("Custom Profiles");
+            ReorderableListGUI.ListField(customProfiles);
 
-			logDebugInfo.boolValue = EditorGUILayout.ToggleLeft( "Log Debug Info", logDebugInfo.boolValue );
-			invertYAxis.boolValue = EditorGUILayout.ToggleLeft( "Invert Y Axis", invertYAxis.boolValue );
-			enableXInput.boolValue = EditorGUILayout.ToggleLeft( "Enable XInput (Windows)", enableXInput.boolValue );
-			useFixedUpdate.boolValue = EditorGUILayout.ToggleLeft( "Use Fixed Update", useFixedUpdate.boolValue );
-			dontDestroyOnLoad.boolValue = EditorGUILayout.ToggleLeft( "Don't Destroy On Load", dontDestroyOnLoad.boolValue );
+            ValidInputProfiles();
 
-			ReorderableListGUI.Title( "Custom Profiles" );
-			ReorderableListGUI.ListField( customProfiles );
+            GUILayout.Space(3.0f);
 
-			GUILayout.Space( 3.0f );
-			
-			serializedObject.ApplyModifiedProperties();
-		}
-	}
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void ValidInputProfiles(){
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var list = new List<Type>();
+            foreach (var assembly in assemblies){
+                foreach (var type in assembly.GetTypes()){
+                    if (type.IsSubclassOf(typeof (UnityInputDeviceProfile))) { list.Add(type); }
+                }
+            }
+            var types = list.ToArray();
+
+            if (types.Any()){
+                var selected = EditorGUILayout.Popup(-1, types.Select(t => t.Name).ToArray());
+                if (selected != -1){
+                    var t = (InControlManager) target;
+                    var selectedName = types.ElementAt(selected).Name;
+                    t.customProfiles.Add(selectedName);
+                }
+            }
+        }
+    }
 }
+
 #endif
